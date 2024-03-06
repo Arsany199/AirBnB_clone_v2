d44f8e5820b075483d49b2546493a806699c94db
arsany199 <arsanykamal30@gmail.com>
2024-03-06 13:04:28 -0800

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,10 +11,10 @@ env.hosts = ['52.201.211.116', '54.210.96.228']
 
 def do_deploy(archive_path):
     """distributes an archive to the web servers"""
-    if exists(archive) is False:
+    if exists(archive_path) is False:
         return False
     try:
-        file_n = archive.split("/")[-1]
+        file_n = archive_path.split("/")[-1]
         no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
