9f43379f2c55a1f998f66cbd2f4f62e77af253b8
arsany199 <arsanykamal30@gmail.com>
2024-03-06 08:12:32 -0800

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
 # script that sets up your web servers for the deployment of web_static
-apt-get -y update
-apt-get -y install nginx
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -hR ubuntu:ubuntu /data/
-sed -i '\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
+apt-get update
+apt-get install -y nginx
+mkdir -p /data/web_static/releases/test
+mkdir -p /data/web_static/shared
+echo "Welcome to AirBnB" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+serve="\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current;\n\t}"
+sed -i "s/^\tserver_name .*;$/&\n$serve/" /etc/nginx/sites-available/default
 service nginx restart
