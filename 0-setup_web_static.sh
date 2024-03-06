2ebe10ce579b468b942abbac438d162195f45819
arsany199 <arsanykamal30@gmail.com>
2024-03-06 08:16:11 -0800

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
-# script that sets up your web servers for the deployment of web_static
-apt-get update
+# prepare the web server for deployment
+apt-get -y update
 apt-get install -y nginx
 mkdir -p /data/web_static/releases/test
 mkdir -p /data/web_static/shared
-echo "Welcome to AirBnB" > /data/web_static/releases/test/index.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current
-chown -R ubuntu:ubuntu /data/
+chown -hR ubuntu:ubuntu /data/
 serve="\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current;\n\t}"
 sed -i "s/^\tserver_name .*;$/&\n$serve/" /etc/nginx/sites-available/default
 service nginx restart
