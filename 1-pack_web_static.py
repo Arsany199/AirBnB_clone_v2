063247403978b7f141fe8919cc3ca5b5738814db
arsany199 <arsanykamal30@gmail.com>
2024-03-06 12:16:53 -0800

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,15 +4,16 @@ from the contents of the web_static"""
 
 from datetime import datetime
 import os
-import fabric
+from fabric.api import *
 
 
 def do_pack():
     """to make archive"""
-    d = datetime.now()
+
     local("sudo mkdir -p versions")
-    archive = "versions/web_static_{}{}{}{}{}{}.tgz"
-                .format(d.year, d.month, d.day, d.hour, d.minute, d.second)
+    d = datetime.now()
+    archive = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+            d.year, d.month, d.day, d.hour, d.minute, d.second)
     res = local("sudo tar -cvzf {} web_static".format(archive))
     if res.failed:
         return None
