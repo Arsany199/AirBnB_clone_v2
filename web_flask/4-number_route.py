f445b80dc3870024ab50c0369bb22e8217a8983b
arsany199 <arsanykamal30@gmail.com>
2024-03-23 15:38:11 -0700

    4-number_route.py

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Sript that starts a Flask web app with 5 routes"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbn():
+    """return Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """function to return HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text_var(text):
+    """function to display text variable passed in"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythontext(text="is cool"):
+    """function displays python with text"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def my_number(n):
+    """return n is a number if it is int"""
+    return "{:d} is a number".format(n)
+
+
+if __name__ == "__main__":
+        app.run(host="0.0.0.0", port=5000)
